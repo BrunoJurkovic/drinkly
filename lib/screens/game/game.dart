9de47cac1b91d9a3683dab01c3e4e9249748db6f
Bruno Jurkovic <bruno.jurkovic1@gmail.com>
2021-03-08 00:08:59 +0100

    Displaying and flipping cards

--- lib/screens/game/game.dart
+++ lib/screens/game/game.dart
@@ -1,5 +1,11 @@
+import 'package:drinkly/models/card.dart';
+import 'package:drinkly/models/deck.dart';
+import 'package:drinkly/services/game_logic.dart';
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:tcard/tcard.dart';
 
 class GameScreen extends StatefulWidget {
   const GameScreen({Key key, this.id}) : super(key: key);
@@ -15,6 +21,10 @@ class _GameScreenState extends State<GameScreen> {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+    Deck deck = Provider.of<GameLogic>(context, listen: false)
+        .getSelectedDeck(widget.id);
+    List<DrinkCard> cards =
+        Provider.of<GameLogic>(context, listen: false).processCards(deck.cards);
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -29,6 +39,91 @@ class _GameScreenState extends State<GameScreen> {
         backgroundColor: Color(0xff2a2438),
       ),
       backgroundColor: Color(0xff2a2438),
+      body: Column(
+        children: [
+          Center(
+            child: TCard(
+              cards: <Widget>[...buildCardItems(cards)],
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  List<Widget> buildCardItems(List<DrinkCard> drinkCards) {
+    List cards = List<Widget>.generate(
+      drinkCards.length,
+      (index) {
+        print(drinkCards[index].type);
+        final height = MediaQuery.of(context).size.height;
+        final width = MediaQuery.of(context).size.width;
+        return FlipCard(
+          key: ValueKey(drinkCards[index].text),
+          flipOnTouch: true,
+          onFlip: () {
+            print('flip');
+          },
+          front: Container(
+            width: width * 0.85,
+            height: height * 0.25,
+            decoration: BoxDecoration(
+              color: drinkCards[index].type == CardType.challenge
+                  ? Colors.pinkAccent[400]
+                  : (drinkCards[index].type == CardType.regular
+                      ? Colors.blueAccent
+                      : (drinkCards[index].type == CardType.rule
+                          ? Colors.green[600]
+                          : (drinkCards[index].type == CardType.competition
+                              ? Colors.red
+                              : Colors.brown))),
+              borderRadius: BorderRadius.all(
+                Radius.circular(25),
+              ),
+            ),
+            child: Center(
+              child: Text(
+                drinkCards[index].type == CardType.challenge
+                    ? 'CHALLENGE'
+                    : (drinkCards[index].type == CardType.regular
+                        ? 'NORMAL'
+                        : (drinkCards[index].type == CardType.rule
+                            ? 'RULE'
+                            : (drinkCards[index].type == CardType.competition
+                                ? 'COMPETITION'
+                                : 'NORMAL'))),
+                style: GoogleFonts.poppins(
+                  fontSize: height * 0.0475,
+                  color: Colors.white.withOpacity(1),
+                  fontWeight: FontWeight.w800,
+                ),
+              ),
+            ),
+          ),
+          back: Container(
+            width: width * 0.85,
+            height: height * 0.25,
+            decoration: BoxDecoration(
+              color: Color(0xff352f44),
+              borderRadius: BorderRadius.all(
+                Radius.circular(25),
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                    padding: EdgeInsets.only(
+                      top: 10,
+                    ),
+                    height: height * 0.1,
+                    width: width * 0.7,
+                    child: Image.asset('assets/images/regular.png')),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+    return cards;
   }
 }
--- lib/services/game_logic.dart
+++ lib/services/game_logic.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+
+import 'package:drinkly/models/card.dart';
 import 'package:drinkly/models/deck.dart';
 import 'package:drinkly/models/player.dart';
 import 'package:flutter/material.dart';
@@ -8,6 +11,82 @@ class GameLogic with ChangeNotifier {
 
   List<Player> get players {
     return _players;
+  }
+
+  List<DrinkCard> processCards(List<DrinkCard> cards) {
+    List<DrinkCard> output = cards;
+    Random random = Random();
+    output.shuffle();
+    for (int i = 0; i <= output.length - 1; i++) {
+      if (output[i].type == CardType.rule) {
+        if ((output.length - i) >= 7) {
+          output.insert(
+            i + (random.nextInt(3) + 2),
+            DrinkCard(
+              text: 'The rule "${output[i].text}" is over.',
+              type: CardType.regular,
+            ),
+          );
+        }
+      }
+    }
+    for (DrinkCard card in output) {
+      if (card.text.contains('{player1}')) {
+        card.text.replaceFirst(
+          RegExp('{player1}'),
+          _players[random.nextInt(_players.length - 1)].name,
+        );
+      }
+      if (card.text.contains('{player1}') && card.text.contains('{player2}')) {
+        String player1 = _players[random.nextInt(_players.length - 1)].name;
+        String player2 = _players[random.nextInt(_players.length - 1)].name;
+        if (player1 != player2) {
+          card.text.replaceFirst(
+            RegExp('{player1}'),
+            player1,
+          );
+          card.text.replaceFirst(
+            RegExp('{player2}'),
+            player2,
+          );
+        } else {
+          if (card.text.contains('{player1}') &&
+              card.text.contains('{player2}')) {
+            String player1 = _players[random.nextInt(_players.length - 1)].name;
+            String player2 = _players[random.nextInt(_players.length - 1)].name;
+            if (player1 != player2) {
+              card.text.replaceFirst(
+                RegExp('{player1}'),
+                player1,
+              );
+              card.text.replaceFirst(
+                RegExp('{player2}'),
+                player2,
+              );
+            } else {
+              if (card.text.contains('{player1}') &&
+                  card.text.contains('{player2}')) {
+                String player1 =
+                    _players[random.nextInt(_players.length - 1)].name;
+                String player2 =
+                    _players[random.nextInt(_players.length - 1)].name;
+                if (player1 != player2) {
+                  card.text.replaceFirst(
+                    RegExp('{player1}'),
+                    player1,
+                  );
+                  card.text.replaceFirst(
+                    RegExp('{player2}'),
+                    player2,
+                  );
+                } else {}
+              }
+            }
+          }
+        }
+      }
+    }
+    return output;
   }
 
   Deck getSelectedDeck(String id) {
